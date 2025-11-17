0f95630f9b03da97f941f0ed1f15dc9babb04083
Mykola <148981382+NazimkovMykola@users.noreply.github.com>
2025-11-17 11:02:20 +0200

    added encoder

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,78 @@
 const char* ssid = "Xiaomi_ANNA";
 const char* password = "23263483";
 
-// const char* radioUrl = "http://direct.fipradio.fr/live/fip-lofi.mp3"; 
-// const char* radioUrl = "http://icecast.omroep.nl:80/3fm-bb-mp3";
-const char* radioUrl = "http://direct.fipradio.fr/live/fip-midfi.mp3";
+const char* radioStations[] = {
+  "http://direct.fipradio.fr/live/fip-midfi.mp3",
+  "http://direct.fipradio.fr/live/fip-lofi.mp3",
+  "http://icecast.omroep.nl:80/3fm-bb-mp3",
+  "http://stream.nbcnews.com/data/news/newsradiolive.mp3"
+  "http://direct.fipradio.fr/live/fip-midfi.mp3",
+"http://direct.fipradio.fr/live/fip-lofi.mp3",
+"http://icecast.omroep.nl:80/3fm-bb-mp3",
+"http://stream.live.vc.bbcmedia.co.uk/bbc_world_service",
+"http://icecast.vrtcdn.be/stubru-high.mp3",
+"http://ice1.somafm.com/groovesalad-256-mp3",
+"http://ice1.somafm.com/defcon-256-mp3",
+"http://ice1.somafm.com/dronezone-256-mp3",
+"http://ice1.somafm.com/spacestation-128-mp3",
+"http://streams.radiobob.de/bob-live/mp3-192",
+"http://icecast.omroep.nl:80/radio1-bb-mp3",
+"http://icecast.omroep.nl:80/radio2-bb-mp3",
+"http://radio.ukr.radio:8000/ur1-mp3",
+"http://radio.ukr.radio:8000/ur2-mp3",
+"http://radio.ukr.radio:8000/ur3-mp3",
+"http://online.radioroks.ua/RadioROKS"
+};
 
+const int numStations = sizeof(radioStations) / sizeof(radioStations[0]);
+int currentStation = 0;
 
-#define I2S_DOUT      22  // DIN на PCM5102A
-#define I2S_BCLK      26  // BCK на PCM5102A  
-#define I2S_LRC       25  // LRCK на PCM5102A
+#define I2S_DOUT      22  
+#define I2S_BCLK      26  
+#define I2S_LRC       25  
+
+#define ENCODER_CLK   12  
+#define ENCODER_DT    14  
 
 Audio audio;
 
+volatile int encoderPos = 0;
+volatile bool encoderChanged = false;
+int lastEncoderPos = 0;
+int lastCLK = HIGH;
+
+void IRAM_ATTR handleEncoder() {
+  int clkState = digitalRead(ENCODER_CLK);
+  int dtState = digitalRead(ENCODER_DT);
+  
+  if (clkState != lastCLK && clkState == LOW) {
+    if (dtState != clkState) {
+      encoderPos++;
+    } else {
+      encoderPos--;
+    }
+    encoderChanged = true;
+  }
+  lastCLK = clkState;
+}
+
+void changeStation(int newStation) {
+  if (newStation < 0) newStation = numStations - 1;
+  if (newStation >= numStations) newStation = 0;
+  
+  currentStation = newStation;
+  
+  audio.stopSong();
+  delay(100);
+  audio.connecttohost(radioStations[currentStation]);
+}
+
 void setup() {
-  Serial.begin(115200);
+  pinMode(ENCODER_CLK, INPUT_PULLUP);
+  pinMode(ENCODER_DT, INPUT_PULLUP);
+  
+  lastCLK = digitalRead(ENCODER_CLK);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), handleEncoder, CHANGE);
   
   WiFi.mode(WIFI_STA);
   WiFi.setSleep(false); 
@@ -24,67 +83,33 @@ void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
-  Serial.println("\nWiFi подключен!");
-  Serial.print("IP адрес: ");
-  Serial.println(WiFi.localIP());
-  Serial.printf("Сила сигнала: %d dBm\n", WiFi.RSSI());
   
   audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-  
   audio.setVolume(21); 
-  audio.setTone(10, 0, 0); 
-  
+  audio.setTone(8, 0, 0); 
   audio.setConnectionTimeout(10000, 30000); 
   audio.setBufsize(-1, 12288);
   
-  delay(2000);
-  Serial.println("Подключение к радиостанции...");
-  if(audio.connecttohost(radioUrl)) {
-    Serial.println("Успешно подключились к радио!");
-    Serial.println("Звук выводится через PCM5102A DAC");
-  } else {
-    Serial.println("Ошибка подключения к радио");
-  }
+  delay(1000);
+  changeStation(currentStation);
 }
 
 void loop() {
-  audio.loop(); 
-}
-
-void audio_info(const char *info){
-  Serial.printf("INFO: %s\n", info);
-}
-
-void audio_id3data(const char *info){
-  Serial.printf("ID3: %s\n", info);
-}
-
-void audio_eof_mp3(const char *info){
-  Serial.printf("EOF: %s\n", info);
-}
-
-void audio_showstation(const char *info){
-  Serial.printf("STATION: %s\n", info);
-}
-
-void audio_showstreamtitle(const char *info){
-  Serial.printf("STREAM TITLE: %s\n", info);
-}
-
-void audio_bitrate(const char *info){
-  Serial.printf("BITRATE: %s\n", info);
-}
-
-void audio_commercial(const char *info){
-  Serial.printf("COMMERCIAL: %s\n", info);
-}
-
-void audio_icyurl(const char *info){
-  Serial.printf("ICY URL: %s\n", info);
-}
-
-void audio_lasthost(const char *info){
-  Serial.printf("LAST HOST: %s\n", info);
+  audio.loop();
+  
+  if (encoderChanged) {
+    encoderChanged = false;
+    
+    int diff = encoderPos - lastEncoderPos;
+    
+    if (abs(diff) >= 4) {
+      if (diff > 0) {
+        changeStation(currentStation + 1);
+      } else {
+        changeStation(currentStation - 1);
+      }
+      lastEncoderPos = encoderPos;
+    }
+  }
 }
