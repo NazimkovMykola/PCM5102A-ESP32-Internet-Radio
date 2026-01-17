e7f1719177d2f1651458456b1fb202631580415b
Mykola <148981382+NazimkovMykola@users.noreply.github.com>
2026-01-17 10:16:25 +0200

    added IR reciever

--- readme.md
+++ readme.md
@@ -1,26 +1,39 @@
 # 24-bit PCM5102A Stereo DAC Digital-to-analog Converter PLL Voice Module pHAT
+
 ## Розпіновка DAC модуля
-* VCC	5V
-* GND	GND
-* BCK	BCK (GPIO26)
-* DIN	OUT (GPIO22)
-* LCK	WS  (GPIO25)
-* FMT	GND
-* XMT	3.3V
-* SCK     (GPIO13)
+
+- VCC 5V
+- GND GND
+- BCK BCK (GPIO26)
+- DIN OUT (GPIO22)
+- LCK WS (GPIO25)
+- FMT GND
+- XMT 3.3V
+- SCK (GPIO13)
 
 ## Розпіновка дисплея
- * SDA 21
- * SCL 23
 
- ## Розпіновка енкодера
-  * S1  12
-  * S2  14
-  * KEY 27
-  
-## Склад системи 
-* ESP32 - контролер
-* PAM-8403 - підсилювач
-* PCM5102A - ЦАП (цифро-аналоговий перетворювач)
-* EC11 - енкодер
+- SDA 21
+- SCL 23
 
+## Розпіновка енкодера
+
+- S1 12
+- S2 14
+- KEY 27
+
+## Розпіновка ІЧ-приймача LF0038M
+
+(відлік ніжок від скошеного краю лінзи)
+
+- 1 - GPIO33
+- 2 - GND
+- 3 - VCC +5V
+
+## Склад системи
+
+- ESP32 - контролер
+- PAM-8403 - підсилювач
+- PCM5102A - ЦАП (цифро-аналоговий перетворювач)
+- EC11 - енкодер
+- ІЧ-приймач LF0038M
--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,24 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 Preferences preferences; 
 Audio audio;
 
+int8_t toneLow = 0, toneMid = 0, toneHigh = 0;
+int currentMode = 0;
+const char* modeNames[] = {"STATION", "BASS", "MIDDLE", "TREBLE"};
+
 bool isMuted = false;
 int lastVolume = 12;
+
+#define ENCODER_CLK 12
+#define ENCODER_DT 14
+#define ENCODER_SW 27
+
+volatile int encoderPos = 0;
+volatile bool encoderChanged = false;
+int lastEncoderPos = 0;
+int lastCLK = HIGH;
+unsigned long buttonDownTime = 0;
+bool buttonActive = false;
+unsigned long lastInteraction = 0; 
 
 struct Station { const char* name; const char* url; };
 Station stationList[] = {
@@ -32,25 +48,9 @@ Station stationList[] = {
     {"3FM BB", "http://icecast.omroep.nl:80/3fm-bb-mp3"},
     {"BBC World Service", "http://stream.live.vc.bbcmedia.co.uk/bbc_world_service"},
     {"Studio Brussel", "http://icecast.vrtcdn.be/stubru-high.mp3"},
-    {"RADIO BOB!", "http://streams.radiobob.de/bob-live/mp3-192"},
     {"Radio ROKS", "http://online.radioroks.ua/RadioROKS"},
-    {"Asura Radio", "https://a9.asurahosting.com:7390/radio.mp3"},
     {"Radio Relax", "http://online.radiorelax.ua/RadioRelax"},
-    {"Melodia FM", "http://online.melodiafm.ua/MelodiaFM"},
-    {"Walm Jazz", "https://icecast.walmradio.com:8443/jazz"},
     {"Frank Sinatra", "https://stream02.pcradio.biz/frank_sinatra-med"},
-    {"Gorgeous FM", "https://listen-gorgeousfm.sharp-stream.com/45_gorgeous_fm_128_mp3"},
-    {"Heart 80s", "https://media-ssl.musicradio.com/Heart80s"},
-    {"Blackout", "https://blimp.streampunk.cc/_stream/blackout.mp3"},
-    {"Wanda FM", "https://icecast.xtvmedia.pp.ua/radiowandafm_hq.mp3"},
-    {"GB News", "https://tunein-live-c.cdnstream1.com/4994_96_2.mp3"},
-    {"ZetCast", "https://cdn1.zetcast.net/stream"},
-    {"104.9 The Wolf", "https://rawlco.leanstream.co/CHUPFM"},
-    {"Shonan Beach FM", "https://shonanbeachfm.out.airtime.pro:8000/shonanbeachfm_a"},
-    {"ROKS Ukraine", "https://online.radioroks.ua/RadioROKS_Ukr"},
-    {"Hit FM Best", "https://online.hitfm.ua/HitFM_Best"},
-    {"Relax Cafe", "https://online.radiorelax.ua/RadioRelax_Cafe"},
-    {"Classic music", "https://s3.radio.co/sa3e464c40/listen"},
     {"Business Radio", "https://cast.brg.ua/business_main_public_mp3_hq"}
 };
 const int numStations = sizeof(stationList) / sizeof(stationList[0]);
@@ -59,45 +59,82 @@ int currentStation = 0;
 int previewStation = 0; 
 String currentTitle = "Ready";
 
+void saveToneSettings() {
+  preferences.begin("radio-cfg", false);
+  preferences.putChar("bass", toneLow);
+  preferences.putChar("mid", toneMid);
+  preferences.putChar("treble", toneHigh);
+  preferences.end();
+}
+
+void IRAM_ATTR handleEncoder() {
+  int clkState = digitalRead(ENCODER_CLK);
+  int dtState = digitalRead(ENCODER_DT);
+  if (clkState != lastCLK && clkState == LOW) {
+    if (dtState != clkState) encoderPos++; else encoderPos--;
+    encoderChanged = true;
+  }
+  lastCLK = clkState;
+}
+
 void updateDisplay() {
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(1);
   display.setCursor(0, 0);
   
-  if (isMuted) {
-    display.print("ST: "); display.print(previewStation + 1);
-    display.setCursor(80, 0);
-    display.print("[MUTE]");
-  } else {
-    display.print("STATION: "); display.print(previewStation + 1);
-    display.print("/"); display.print(numStations);
-  }
+  display.print("MODE: "); display.print(modeNames[currentMode]);
+  if (isMuted) { display.setCursor(90, 0); display.print("[M]"); }
 
   display.drawLine(0, 12, 128, 12, WHITE);
   display.setCursor(0, 20);
-  display.println(stationList[previewStation].name);
-  display.setCursor(0, 40);
-  display.println(currentTitle);
-  
+
+  if (currentMode == 0) {
+    display.print("ST: "); display.print(previewStation + 1);
+    display.print("/"); display.println(numStations);
+    display.setCursor(0, 32);
+    display.println(stationList[previewStation].name);
+    display.setCursor(0, 50);
+    display.println(currentTitle);
+  } else {
+    int8_t val = (currentMode == 1) ? toneLow : (currentMode == 2) ? toneMid : toneHigh;
+    display.setTextSize(2);
+    display.setCursor(35, 30);
+    display.printf("%+d dB", val);
+    display.drawRect(10, 55, 108, 6, WHITE);
+    int barWidth = map(val, -10, 6, 0, 104);
+    display.fillRect(12, 57, barWidth, 2, WHITE);
+  }
   display.display();
 }
 
 void setup() {
-  Serial.begin(115200);
-  
-  Wire.begin(I2C_SDA, I2C_SCL);
+  Wire.begin(21, 23);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) for(;;);
 
   irrecv.enableIRIn(); 
+
+  pinMode(ENCODER_CLK, INPUT_PULLUP);
+  pinMode(ENCODER_DT, INPUT_PULLUP);
+  pinMode(ENCODER_SW, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), handleEncoder, CHANGE);
+
+  preferences.begin("radio-cfg", true);
+  toneLow = preferences.getChar("bass", 0);
+  toneMid = preferences.getChar("mid", 0);
+  toneHigh = preferences.getChar("treble", 0);
+  currentStation = preferences.getInt("station_idx", 0);
+  preferences.end();
 
   WiFi.begin("Xiaomi_ANNA", "23263483");
   while (WiFi.status() != WL_CONNECTED) delay(500);
 
   audio.setPinout(26, 25, 22);
   audio.setVolume(lastVolume);
-  audio.connecttohost(stationList[currentStation].url);
+  audio.setTone(toneLow, toneMid, toneHigh);
   
+  previewStation = currentStation;
+  audio.connecttohost(stationList[currentStation].url);
   updateDisplay();
 }
 
@@ -106,44 +143,92 @@ void loop() {
 
   if (irrecv.decode(&results)) {
     uint32_t code = (uint32_t)results.value;
-    
-    if (code == 0x11EECC33 || code == 0x11EEA857) { // Next
-       previewStation = (previewStation + 1) % numStations;
-       currentStation = previewStation;
-       audio.connecttohost(stationList[currentStation].url);
-    } 
-    else if (code == 0x11EE2CD3 || code == 0x11EE6897) { // Prev
-       previewStation = (previewStation - 1 + numStations) % numStations;
-       currentStation = previewStation;
-       audio.connecttohost(stationList[currentStation].url);
+    lastInteraction = millis(); 
+
+    if (code == 0x11EE08F7) { 
+       currentMode = (currentMode + 1) % 4;
     }
-    else if (code == 0x20EF906F) { 
-       isMuted = !isMuted;
-       if (isMuted) {
-         audio.setVolume(0);
+    else if (code == 0x11EECC33 || code == 0x11EEA857) { 
+       if (currentMode == 0) { 
+         previewStation = (previewStation + 1) % numStations;
+         currentStation = previewStation;
+         audio.connecttohost(stationList[currentStation].url);
        } else {
-         audio.setVolume(lastVolume);
+         if (currentMode == 1) toneLow = constrain(toneLow + 1, -10, 6);
+         else if (currentMode == 2) toneMid = constrain(toneMid + 1, -10, 6);
+         else if (currentMode == 3) toneHigh = constrain(toneHigh + 1, -10, 6);
+         audio.setTone(toneLow, toneMid, toneHigh);
+         saveToneSettings();
+       }
+    } 
+    else if (code == 0x11EE2CD3 || code == 0x11EE6897) {
+       if (currentMode == 0) { 
+         previewStation = (previewStation - 1 + numStations) % numStations;
+         currentStation = previewStation;
+         audio.connecttohost(stationList[currentStation].url);
+       } else {
+         if (currentMode == 1) toneLow = constrain(toneLow - 1, -10, 6);
+         else if (currentMode == 2) toneMid = constrain(toneMid - 1, -10, 6);
+         else if (currentMode == 3) toneHigh = constrain(toneHigh - 1, -10, 6);
+         audio.setTone(toneLow, toneMid, toneHigh);
+         saveToneSettings();
        }
     }
-
+    else if (code == 0x20DF906F || code == 0x11EE9867) { 
+       isMuted = !isMuted;
+       audio.setVolume(isMuted ? 0 : lastVolume);
+    }
     updateDisplay();
     irrecv.resume(); 
   }
 
-  static unsigned long lastUpdate = 0;
-  if (millis() - lastUpdate > 2000) {
-    lastUpdate = millis();
+  int btnState = digitalRead(ENCODER_SW);
+  if (btnState == LOW && !buttonActive) {
+    buttonActive = true;
+    buttonDownTime = millis();
+  }
+  if (btnState == HIGH && buttonActive) {
+    buttonActive = false;
+    lastInteraction = millis();
+    if (millis() - buttonDownTime > 500) { 
+       currentMode = (currentMode + 1) % 4;
+    } else { 
+       if (currentMode == 0) {
+         currentStation = previewStation;
+         audio.connecttohost(stationList[currentStation].url);
+       }
+    }
+    updateDisplay();
+  }
+
+  if (encoderChanged) {
+    encoderChanged = false;
+    lastInteraction = millis();
+    int diff = encoderPos - lastEncoderPos;
+    if (abs(diff) >= 2) {
+      int dir = (diff > 0) ? 1 : -1;
+      if (currentMode == 0) {
+        previewStation = (previewStation + dir + numStations) % numStations;
+      } else {
+        if (currentMode == 1) toneLow = constrain(toneLow + dir, -10, 6);
+        else if (currentMode == 2) toneMid = constrain(toneMid + dir, -10, 6);
+        else if (currentMode == 3) toneHigh = constrain(toneHigh + dir, -10, 6);
+        audio.setTone(toneLow, toneMid, toneHigh);
+        saveToneSettings();
+      }
+      lastEncoderPos = encoderPos;
+      updateDisplay();
+    }
+  }
+
+  if (currentMode != 0 && millis() - lastInteraction > 10000) {
+    currentMode = 0;
     updateDisplay();
   }
 }
 
 void audio_showstreamtitle(const char *i) { 
-  String s = String(i);
-  s.trim();
-  if (s.length() > 0) {
-    currentTitle = s;
-    updateDisplay(); 
-  }
+  String s = String(i); s.trim();
+  if (s.length() > 0) { currentTitle = s; updateDisplay(); }
 }
-void audio_info(const char *i) {}
 void audio_error(const char *i) { currentTitle = "Stream Error"; updateDisplay(); }
